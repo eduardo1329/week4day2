74ec2f787e6bbdb84cb1c28a8582df0b2e4b206a
admoreno3 <142839210+admoreno3@users.noreply.github.com>
2023-09-13 19:55:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Eduardo Romo
+# adelynn moreno
+name = input('what is your name?')
+sold = float(input('How much have you sold this month?'))
+comissions = (sold * 13 / 100)
+print(f'Hello {name} you have earned ${comissions} this month.')
